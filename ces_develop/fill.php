1e7af5f5bd03f8ed7008b7b307d9f7cd8bfe9eff
Esteve <badia.orive@gmail.com>
2012-05-19 18:03:08 +0200

    Standarized code.

--- ces_develop/fill.php
+++ ces_develop/fill.php
@@ -1,32 +1,32 @@
 <?php
 /**
- * This file is a script for filling the database with initial data for 
- * developing purposes. 
- * 
+ * @file
+ * This file is a script for filling the database with initial data for
+ * developing purposes.
+ *
  * It deletes all ces/bank data.
- * 
+ *
  * It creates a pair of exchanges and several accounts as detailed:
- * 
- * Exchange: HORA 
+ *
+ * Exchange: HORA
  *  Account: HORA0001
  *  Account: HORA0002
  * Exchange: BCNA
  *  Account: BCNA0001
  *  Account: BCNA0002
- * 
+ *
  * And the following users if they don't exist
- * 
+ *
  * adminhora (HORA administrator)
  * userhora (HORA0001 owner)
  *
  * adminbcna (BCNA administrator)
  * userbcna (BCNA0001 owner)
- * 
+ *
  * userhorabcna (HORA0002 & BCNA0002 owner)
- * 
- * 
+ *
+ *
  */
-
 //clean
 db_delete('ces_account')->execute();
 db_delete('ces_accountuser')->execute();
@@ -37,22 +37,20 @@ db_delete('ces_messages')->execute();
 db_delete('ces_permission')->execute();
 db_delete('ces_transaction')->execute();
 include_once DRUPAL_ROOT . '/includes/install.inc';
-require_once(drupal_get_path('module', 'bank').'/bank.install');
+require_once(drupal_get_path('module', 'bank') . '/bank.install');
 _bank_create_default_exchange();
 _bank_create_default_permissions();
-
 //create users
-registerUser('adminhora');
-registerUser('userhora');
-registerUser('adminbcna');
-registerUser('userbcna');
-registerUser('userhorabcna');
+register_user('adminhora');
+register_user('userhora');
+register_user('adminbcna');
+register_user('userbcna');
+register_user('userhorabcna');
 $adminhora = user_load_by_name('adminhora');
 $userhora = user_load_by_name('userhora');
 $adminbcna = user_load_by_name('adminbcna');
 $userbcna = user_load_by_name('userbcna');
 $userhorabcna = user_load_by_name('userhorabcna');
-
 //create exchanges
 $bank = new Bank();
 $hora = array(
@@ -73,7 +71,6 @@ $hora = array(
 );
 $bank->createExchange($hora);
 $bank->activateExchange($hora);
-
 $bcna = array(
   'code' => 'BCNA',
   'shortname' => 'EX Barcelona',
@@ -95,30 +92,30 @@ $bank->activateExchange($bcna);
 //create accounts
 $limitchainhora = $bank->getDefaultLimitChain($hora['id']);
 $hora0001 = array(
-  'exchange'=>$hora['id'],
-  'name'=>'HORA0001',
-  'limitchain'=>$limitchainhora['id'],
-  'kind'=>LocalAccount::TYPE_INDIVIDUAL,
-  'state'=>LocalAccount::STATE_HIDDEN,
-  'users'=>array(
+  'exchange' => $hora['id'],
+  'name' => 'HORA0001',
+  'limitchain' => $limitchainhora['id'],
+  'kind' => LocalAccount::TYPE_INDIVIDUAL,
+  'state' => LocalAccount::STATE_HIDDEN,
+  'users' => array(
     array(
-      'user'=>$userhora->uid,
-      'role'=>AccountUser::ROLE_ACCOUNT_ADMINISTRATOR,
+      'user' => $userhora->uid,
+      'role' => AccountUser::ROLE_ACCOUNT_ADMINISTRATOR,
     )
   )
 );
 $bank->createAccount($hora0001);
 $bank->activateAccount($hora0001);
 $hora0002 = array(
-  'exchange'=>$hora['id'],
-  'name'=>'HORA0002',
-  'limitchain'=>$limitchainhora['id'],
-  'kind'=>LocalAccount::TYPE_INDIVIDUAL,
-  'state'=>LocalAccount::STATE_ACTIVE,
-  'users'=>array(
+  'exchange' => $hora['id'],
+  'name' => 'HORA0002',
+  'limitchain' => $limitchainhora['id'],
+  'kind' => LocalAccount::TYPE_INDIVIDUAL,
+  'state' => LocalAccount::STATE_ACTIVE,
+  'users' => array(
     array(
-      'user'=>$userhorabcna->uid,
-      'role'=>AccountUser::ROLE_ACCOUNT_ADMINISTRATOR,
+      'user' => $userhorabcna->uid,
+      'role' => AccountUser::ROLE_ACCOUNT_ADMINISTRATOR,
     )
   )
 );
@@ -126,48 +123,44 @@ $bank->createAccount($hora0002);
 $bank->activateAccount($hora0002);
 $limitchainbcna = $bank->getDefaultLimitChain($bcna['id']);
 $bcna0001 = array(
-  'exchange'=>$bcna['id'],
-  'name'=>'BCNA0001',
-  'limitchain'=>$limitchainbcna['id'],
-  'kind'=>LocalAccount::TYPE_INDIVIDUAL,
-  'state'=>LocalAccount::STATE_ACTIVE,
-  'users'=>array(
+  'exchange' => $bcna['id'],
+  'name' => 'BCNA0001',
+  'limitchain' => $limitchainbcna['id'],
+  'kind' => LocalAccount::TYPE_INDIVIDUAL,
+  'state' => LocalAccount::STATE_ACTIVE,
+  'users' => array(
     array(
-      'user'=>$userbcna->uid,
-      'role'=>AccountUser::ROLE_ACCOUNT_ADMINISTRATOR,
+      'user' => $userbcna->uid,
+      'role' => AccountUser::ROLE_ACCOUNT_ADMINISTRATOR,
     )
   )
 );
 $bank->createAccount($bcna0001);
 $bank->activateAccount($bcna0001);
 $bcna0002 = array(
-  'exchange'=>$bcna['id'],
-  'name'=>'BCNA0002',
-  'limitchain'=>$limitchainbcna['id'],
-  'kind'=>LocalAccount::TYPE_INDIVIDUAL,
-  'state'=>LocalAccount::STATE_ACTIVE,
-  'users'=>array(
+  'exchange' => $bcna['id'],
+  'name' => 'BCNA0002',
+  'limitchain' => $limitchainbcna['id'],
+  'kind' => LocalAccount::TYPE_INDIVIDUAL,
+  'state' => LocalAccount::STATE_ACTIVE,
+  'users' => array(
     array(
-      'user'=>$userhorabcna->uid,
-      'role'=>AccountUser::ROLE_ACCOUNT_ADMINISTRATOR,
+      'user' => $userhorabcna->uid,
+      'role' => AccountUser::ROLE_ACCOUNT_ADMINISTRATOR,
     )
   )
 );
 $bank->createAccount($bcna0002);
 $bank->activateAccount($bcna0002);
-
-
-function registerUser($name){
+function register_user($name) {
   // register a new user
-  if(!user_load_by_name($name)){
+  if (!user_load_by_name($name)) {
     $form_state = array();
     $form_state['values']['name'] = $name;
-    $form_state['values']['mail'] = $name.'@test.com';
+    $form_state['values']['mail'] = $name . '@test.com';
     $form_state['values']['pass']['pass1'] = 'password';
     $form_state['values']['pass']['pass2'] = 'password';
     $form_state['values']['op'] = t('Create new account');
     drupal_form_submit('user_register_form', $form_state);
   }
 }
-
-
