2b73b7d97e0432d6b78cc5cddfde3004b5e9f03d
Esteve <badia.orive@gmail.com>
2012-12-13 20:11:05 +0100

    Big commit 13-dec-2012

--- ces_develop/demo.php
+++ ces_develop/demo.php
@@ -8,6 +8,7 @@
  */
 include_once DRUPAL_ROOT . '/includes/install.inc';
 require_once(drupal_get_path('module', 'ces_bank') . '/ces_bank.install');
+require_once(drupal_get_path('module', 'ces_offerswants') . '/ces_offerswants.module');
 //RESET USERS
 $query = db_query('SELECT uid FROM {users}');
 $users = $query->fetchAllAssoc('uid');
@@ -25,13 +26,15 @@ db_delete('ces_limitchain')->execute();
 db_delete('ces_messages')->execute();
 db_delete('ces_permission')->execute();
 db_delete('ces_transaction')->execute();
+db_delete('ces_offerwant')->execute();
+db_delete('ces_category')->execute();
 _ces_bank_create_default_exchange();
 _ces_bank_create_default_permissions();
 //CREATE STUFF
 $bank = new Bank();
 
 //CREATE USERS
-$usernames = array('adminnet1', 'adminnet2', 'Gauss', 'Euler', 'Riemann', 'Euclides', 'Noether', 'Galois', 'Hamilton', 'Newton');
+$usernames = array('Riemann', 'Euclides', 'Gauss' , 'Noether', 'Fermat');
 $users = array();
 foreach ($usernames as $name) {
   $users[$name] = register_user($name);
@@ -40,7 +43,7 @@ foreach ($usernames as $name) {
 $net1 = array(
   'code' => 'NET1',
   'shortname' => 'Net 1',
-  'name' => 'Exchange network 1 - Time bank',
+  'name' => 'Network 1 - Time bank',
   'website' => 'http://www.integralces.net',
   'country' => 'ES',
   'region' => 'Bages',
@@ -51,14 +54,14 @@ $net1 = array(
   'currenciesname' => 'hours',
   'currencyvalue' => '1.0',
   'currencyscale' => '2',
-  'admin' => $users['adminnet1']->uid,
+  'admin' => $users['Riemann']->uid,
 );
 $bank->createExchange($net1);
 $bank->activateExchange($net1);
 $net2 = array(
   'code' => 'NET2',
   'shortname' => 'Net 2',
-  'name' => 'Exchange network 2 - Euro based',
+  'name' => 'Network 2 - Euro based',
   'website' => 'http://www.integralces.net',
   'country' => 'ES',
   'region' => 'Barcelonès',
@@ -69,45 +72,45 @@ $net2 = array(
   'currenciesname' => 'ecos',
   'currencyvalue' => '0.1',
   'currencyscale' => '2',
-  'admin' => $users['adminnet2']->uid,
+  'admin' => $users['Fermat']->uid,
 );
 $bank->createExchange($net2);
 $bank->activateExchange($net2);
 //CREATE ACCOUNTS
 $accounts = array();
-for ($i=0; $i<4; $i++) {
-  $name = $usernames[$i+2];
+for ($i=0; $i<3; $i++) {
+  $name = $usernames[$i];
   $accounts[$name] = register_account($users[$name], $net1, $i+1);
 }
-for ($i=0; $i<4; $i++) {
-  $name = $usernames[$i+6];
+for ($i=0; $i<3; $i++) {
+  $name = $usernames[$i+2];
   $accounts[$name] = register_account($users[$name], $net2, $i+1);
 }
 //CREATE LOCAL TRANSACTIONS
-
-for ($e=1; $e<=2; $e++) {
-  for ($i=0; $i<10; $i++) {
-    $from = rand(1, 4);
-    do{
-      $to = rand(1, 4);
-    }while($from == $to);
-    $trans = array(
-        'fromaccountname' => 'NET' . $e . '000' . $from,
-        'toaccountname' => 'NET' . $e . '000' . $to,
-        'amount' => rand(1, 100)/10,
-        'concept' => 'NET' . $e . ' demo transaction #' . $i
-      );
-    $bank->createTransaction($trans);
-    $bank->applyTransaction($trans['id']);
-  }
+$transactions = array(
+    array('NET10001', 'NET10002', 1.2, '3kg of potatoes.'),
+    array('NET10002', 'NET10003', 0.8, 'Standard haircut.'),
+    array('NET10003', 'NET10001', 2.1, 'Yearly website mantainment'),
+    array('NET20001', 'NET20002', 25, 'Bike revision.'),
+    array('NET20002', 'NET20003', 6, 'Natural soap.'),
+    array('NET20003', 'NET20001', 5.5, 'Ecologic carrots'),
+);
+foreach($transactions as $t) {
+  $trans = array(
+    'fromaccountname' => $t[0],
+    'toaccountname' => $t[1],
+    'amount' => $t[2],
+    'concept' => $t[3],
+  );
+  $bank->createTransaction($trans);
+  $bank->applyTransaction($trans['id']);
 }
-
 //CREATE ONE INTER-TRANSACTION
 $trans = array(
   'fromaccountname' => 'NET10001',
   'toaccountname' => 'NET20001',
   'amount' => '10',
-  'concept' => 'Inter-exchange demo transaction #1',
+  'concept' => 'Some old math books from Germany.',
 );
 $bank->createTransaction($trans);
 $bank->applyTransaction($trans['id']);
@@ -119,6 +122,105 @@ $bank->activateAccount($account);
 //RE-TRIGGER INTEREXCHANGE TRANSACTION
 $bank->applyTransaction($trans['id']);
 
+//
+// OFFERWANTS
+//
+
+//Add categories
+
+$names = array('Food', 'Hygiene', 'Professional services', 'Reparation', 'Education');
+$exchanges = array($net1, $net2);
+$categories = array();
+foreach($exchanges as $e){
+  $categories[$e['id']] = array();
+  foreach($names as $c){
+    $cat = array(
+      'parent' => 0,
+      'title' => $c,
+      'description' => $c,
+      'exchange'=> $e['id'],
+      'context' => 1,
+    );
+    $categories[$e['id']][$c] = ces_category_save((object)$cat);
+  }
+}
+
+//Add some offers 
+
+$offers = array(array(
+  'type'=>'offer',
+  'user'=>$users['Riemann']->uid, 
+  'title'=>'Cow\'s milk', 
+  'body'=>'Natural cow\'s milk. Probably the best you\'ve ever tasted.',
+  'category'=>$categories[$net1['id']]['Food']->id,
+  'keywords'=>'',
+  'state'=>1,
+  'created' => time(),
+  'modified' => time(),
+  'expire' => time()+3600*24*365,
+),array(
+  'type'=>'offer',
+  'user'=>$users['Euclides']->uid, 
+  'title'=>'Bicycle mechanic', 
+  'body'=>'I fix or setup your bike in less than an hour.',
+  'category'=>$categories[$net1['id']]['Reparation']->id,
+  'keywords'=>'',
+  'state'=>1,
+  'created' => time(),
+  'modified' => time(),
+  'expire' => time()+3600*24*365,
+),array(
+  'type'=>'offer',
+  'user'=>$users['Gauss']->uid, 
+  'title'=>'Natural soap', 
+  'body'=>'Natural soap with smell of Alpine flowers. Very good for your skin.',
+  'category'=>$categories[$net1['id']]['Hygiene']->id,
+  'keywords'=>'',
+  'state'=>1,
+  'created' => time(),
+  'modified' => time(),
+  'expire' => time()+3600*24*365,
+),array(
+  'type'=>'offer',
+  'user'=>$users['Gauss']->uid, 
+  'title'=>'Cow\'s milk', 
+  'body'=>'Natural sheep\'s milk. Probably the best you\'ve ever tasted.',
+  'category'=>$categories[$net2['id']]['Food']->id,
+  'keywords'=>'',
+  'state'=>1,
+  'created' => time(),
+  'modified' => time(),
+  'expire' => time()+3600*24*365,
+),array(
+  'type'=>'offer',
+  'user'=>$users['Noether']->uid, 
+  'title'=>'Car mechanic', 
+  'body'=>'I fix or setup your car in less than an hour.',
+  'category'=>$categories[$net2['id']]['Reparation']->id,
+  'keywords'=>'',
+  'state'=>1,
+  'created' => time(),
+  'modified' => time(),
+  'expire' => time()+3600*24*365,
+),array(
+  'type'=>'offer',
+  'user'=>$users['Fermat']->uid, 
+  'title'=>'Natural shampoo', 
+  'body'=>'Natural shampoo with smell of Pyrinee flowers. Very good for your hair.',
+  'category'=>$categories[$net2['id']]['Hygiene']->id,
+  'keywords'=>'',
+  'state'=>1,
+  'created' => time(),
+  'modified' => time(),
+  'expire' => time()+3600*24*365,
+));
+foreach($offers as $offer) {
+  $o = (object)$offer;
+  ces_offerwant_save($o);
+  
+}
+
+
 function register_user($name) {
   // register a new user
   if (!user_load_by_name($name)) {
@@ -127,6 +229,10 @@ function register_user($name) {
     $form_state['values']['mail'] = strtolower($name) . '@integralces.net';
     $form_state['values']['pass']['pass1'] = 'integralces';
     $form_state['values']['pass']['pass2'] = 'integralces';
+    $form_state['values']['ces_firstname']['und'][0]['value'] = $name;
+    $form_state['values']['ces_town']['und'][0]['value'] = 'Somewhere';
+    $form_state['values']['ces_postcode']['und'][0]['value'] = '12345';
+    $form_state['values']['ces_phonemobile']['und']['0']['value'] = '123456789';
     $form_state['values']['op'] = t('Create new account');
     drupal_form_submit('user_register_form', $form_state);
   }
---
+++ ces_offerswants/ces_offerwant.tpl.php
@@ -0,0 +1,55 @@
+<div class="ces-offerwant">
+  <div class="ces-offerwant-display" >
+    <div class="ces-offerwant-title"><?php echo $ces_offerwant_title; ?></div>
+    <div class="ces-offerwant-content">
+      <div class="ces-offerwant-image"><?php 
+      if (!empty($ces_offerwant_image)):
+        $ces_offerwant_image['width'] = '200px';
+        echo theme('image', $ces_offerwant_image); 
+      endif;
+      ?></div>
+      <div class="ces-offerwant-body"><?php echo $ces_offerwant_body; ?></div>
+    </div>
+  </div>
+  <?php if ($view_mode_full): ?>
+  <div class="ces-offerwant-properties">
+    <dl class="ces-offerwant-properties-list">
+      <dt><?php echo t('Category'); ?></dt>
+      <dd><?php echo $ces_offerwant_category; ?></dd>
+      <dt><?php echo t('Status'); ?></dt>
+      <dd><?php echo $ces_offerwant_state; ?></dd>
+      <dt><?php echo t('Updated'); ?></dt>
+      <dd><?php echo $ces_offerwant_modified; ?></dd>
+      <dt><?php echo t('Expire'); ?></dt>
+      <dd><?php echo $ces_offerwant_expire; ?></dd>
+      <dt><?php echo t('Keywords'); ?></dt>
+      <dd><?php echo $ces_offerwant_keywords; ?></dd>
+    </dl>
+  </div>
+  <?php endif; ?>
+  <div class="ces-offerwant-footer">
+    <div class="ces-offerwant-seller">
+      <?php global $base_path; ?>
+      <div class="ces-offerwant-seller-picture">
+        <a href="<?php print $base_path; ?>user/<?php echo $ces_offerwant_seller_uid; ?>"  title="<?php echo t('Seller\'s info'); ?>">
+      <?php print theme('user_picture', array('account' => user_load($ces_offerwant_seller_uid))); ?>
+        </a>
+      </div>
+      <div class="ces-offerwant-seller-name-and-phone">
+        <div class="ces-offerwant-seller-name">
+          <a href="<?php print $base_path; ?>user/<?php echo $ces_offerwant_seller_uid; ?>" title="<?php echo t('Seller\'s info'); ?>">
+          <?php echo $ces_offerwant_seller_name; ?>
+          </a>
+        </div>
+        <div class="ces-offerwant-seller-phone">
+          <?php echo $ces_offerwant_seller_phone; ?>
+        </div>
+      </div>
+    </div>
+    <div class="ces-offerwant-actions">
+      <?php echo $ces_offerwant_actions; ?>
+    </div>
+    <div class="clearfix"/>
+  </div>
+  
+</div>
