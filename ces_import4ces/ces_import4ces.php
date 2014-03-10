be08604bf5079639e2a8c32be2698208cd7ef98e
edumag <eduardo@mamedu.com>
2014-03-10 17:56:16 +0100

    Create base module

---
+++ ces_import4ces/ces_import4ces.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @file
+ * Import data from CES
+ */
+
+include_once DRUPAL_ROOT . '/includes/install.inc';
+require_once drupal_get_path('module', 'ces_bank') . '/ces_bank.install';
+require_once drupal_get_path('module', 'ces_offerswants') . '/ces_offerswants.module';
+
+echo "Hola mundo";
