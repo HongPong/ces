ab2ac8897bf5165624dd334f44588ece4de95fc7
esteve <badia.orive@gmail.com>
2014-03-12 21:56:20 +0100

    Issue #2216627: Index documentation page and development environment in spanish

---
+++ docs/es/index.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @mainpage Documentación para desarrolladores
+ * Bienvenido o bienvenida a la documentación para desarrolladores del proyecto
+ * IntegralCES. Ésta es una documentación permanentemente en construcción que
+ * vamos completando a medida que nos es necesario.
+ * 
+ * Algunos componentes de IntegralCES:
+ * - @link ces_dev_install Entorno de desarrollo @endlink
+ *   - @link ces_dev_git Git flow @endlink
+ * - @link ces Módulo raíz @endlink
+ *   - @link ces_database Base de datos @endlink
+ * - @link ces_bank Módulo bancario @endlink
+ *   - @link ces_bank_transactons Transacciones @endlink
+ *   - @link ces_bank_interexchange_transactions Transacciones entre grupos @endlink
+ */
