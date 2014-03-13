07e1aa92096a0efa1c74752222d8d3fcee794059
esteve <badia.orive@gmail.com>
2014-03-13 18:59:32 +0100

    cahnged link name

--- docs/es/index.php
+++ docs/es/index.php
@@ -7,11 +7,14 @@
  * vamos completando a medida que nos es necesario.
  * 
  * Algunos componentes de IntegralCES:
- * - @link ces_dev_install Entorno de desarrollo @endlink
- *   - @link ces_dev_git Git flow @endlink
+ * - @link ces_dev_install.html Entorno de desarrollo @endlink
+ *   - @link ces_dev_flow.html Flujo de desarrollo @endlink
  * - @link ces Módulo raíz @endlink
  *   - @link ces_database Base de datos @endlink
  * - @link ces_bank Módulo bancario @endlink
  *   - @link ces_bank_transactons Transacciones @endlink
  *   - @link ces_bank_interexchange_transactions Transacciones entre grupos @endlink
+ * - @link ces_import Módulo de importación @endlink
+ *   - @link ces_migration_data Datos de importación del CES @endlink
+ * 
  */
